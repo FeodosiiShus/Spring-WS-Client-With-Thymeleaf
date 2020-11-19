16257e8e310775b95e3c064950e962368dab5d7e
FeodosiiShus <oleksandr.kreminskyi@nure.ua>
2020-11-19 16:13:06 +0200

    Start develop web-pages for SOAP-service

---
+++ src/main/java/ua/nure/inettech/controller/GymController.java
@@ -0,0 +1,16 @@
+package ua.nure.inettech.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping(path = "/")
+public class GymController {
+
+    @GetMapping("gyms")
+    public String getAllGyms(){
+        return "gym";
+    }
+}
