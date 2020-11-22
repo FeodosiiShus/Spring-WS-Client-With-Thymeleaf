f31ac327d6172d1116d0b0e4183db1fd3e7f0630
FeodosiiShus <oleksandr.kreminskyi@nure.ua>
2020-11-22 12:26:09 +0200

    Add navbar and index page

--- src/main/java/ua/nure/inettech/controller/GymController.java
+++ src/main/java/ua/nure/inettech/controller/GymController.java
@@ -5,11 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-import ua.nure.inettech.entity.Gyms;
 import ua.nure.inettech.repository.GymRepository;
 
-import java.util.Collections;
 
 
 @Controller
